34a11cbdd7898ea4c48bfb055580f87bf73189e5
jdboachie <judeboachiekwame@gmail.com>
2023-04-26 02:47:24 +0000

    Update sort.h

--- sort.h
+++ sort.h
@@ -5,20 +5,14 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
+
 /**
  * struct listint_s - Doubly linked list node
- * 
+ *
  * @n: Integer stored in the node
  * @prev: Pointer to the previous element of the list
  * @next: Pointer to the next element of the list
-*/
-typedef struct listint_s
-{
-	const int n;
-	struct listint_s *prev;
-	struct listint_s *next;
-} listint_s;
-
+ */
 typedef struct listint_s
 {
 	const int n;
