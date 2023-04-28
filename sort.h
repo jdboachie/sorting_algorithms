989249fd7ef63e1853ffe973c917912d64e4ace8
jdboachie <judeboachiekwame@gmail.com>
2023-04-28 13:07:53 +0000

    Update sort.h

--- sort.h
+++ sort.h
@@ -28,7 +28,7 @@ void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
-listint_t *create_listint(const int *array, size_t size);;
+listint_t *create_listint(const int *array, size_t size);
 
 
 #endif /* SORT_H */
