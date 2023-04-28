198157e708cf31c18099cf9f03fab4bab50a98d8
jdboachie <judeboachiekwame@gmail.com>
2023-04-28 12:37:54 +0000

    Update 0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -32,9 +32,9 @@ void bubble_sort(int *array, size_t size)
 	{
 		swapped = false;
 
-		for (i = 0; i <= size - 1; i++)
+		for (i = 0; i < (size - 1); i++)
 		{
-			for (j = 0; j < size - 1; j++)
+			for (j = 0; j < (size - 1); j++)
 			{
 				if (array[j] > array[j + 1])
 				{
