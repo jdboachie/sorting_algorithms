08d436a2530e4e0d2790134cab4b9aaeb4f1b829
jdboachie <judeboachiekwame@gmail.com>
2023-04-28 13:06:10 +0000

    Add updates for insertion sort task

--- sort.h
+++ sort.h
@@ -21,7 +21,14 @@ typedef struct listint_s
 } listint_t;
 
 void bubble_sort(int *array, size_t size);
+void swap_ints(int *a, int *b);
+void insertion_sort_list(listint_t **list);
+void swap_nodes(listint_t **list, listint_t **node1, listint_t **node2);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+listint_t *create_listint(const int *array, size_t size);;
 
 
 #endif /* SORT_H */
